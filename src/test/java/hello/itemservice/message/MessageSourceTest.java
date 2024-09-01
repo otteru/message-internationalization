31d48c0b58bc2c681d76365de8cb07e4ba54a687
otteru <otteru@naver.com>
2024-09-01 10:56:25 +0900

    first commit test 인코딩 문제 있음

---
+++ src/test/java/hello/itemservice/message/MessageSourceTest.java
@@ -0,0 +1,21 @@
+package hello.itemservice.message;
+
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.MessageSource;
+
+@SpringBootTest
+public class MessageSourceTest {
+
+    @Autowired
+    MessageSource ms;
+
+    @Test
+    void helloMessage() {
+        String result = ms.getMessage("hello", null, null);// 뒤의 2개는 argument, locale 정보이다.
+        Assertions.assertThat(result).isEqualTo("안녕");
+    }
+
+}
